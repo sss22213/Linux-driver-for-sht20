0c780599c97b847b9fc8fdc1055a4e70398391bf
sss22213 <n0404.n0404@gmail.com>
2021-04-06 15:34:21 +0000

    - Add display for temperature and humidity on test program

--- test.c
+++ test.c
@@ -5,7 +5,13 @@
 
 int main()
 {
+    float temperature = 0;
+    
+    float humidity = 0;
+
     unsigned char buffer[4] = {0x00};
+
+    /* Read info from sht20 */
     int fd = open("/dev/sht20", O_RDWR);
     if (fd < 0) {
     	printf("Error: cannot open file.\n");
@@ -13,7 +19,15 @@ int main()
     }
     printf("Open success\n");
     read(fd, &buffer, 4);
-    printf("0x%x 0x%x 0x%x 0x%x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
     close(fd);
+
+    printf("Origin: 0x%x 0x%x 0x%x 0x%x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+
+    temperature = (buffer[0] << 8 | buffer[1]) / 100.0;
+
+    humidity = (buffer[2] << 8 | buffer[3]) / 100.0;
+
+    printf("Temperature: %.2f, Humidity: %.2f\n", temperature, humidity);
+
     return 0;
 }
